574774ba29ccd661154431d5600240f090440c37
Lorenzo Brown <lorenzo@udacity.com>
2015-03-11 14:08:02 -0700

    Update pg_config.sh
    Added installs for Auth&Auth

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -4,6 +4,9 @@ apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 pip install bleach
+pip install oauth2client
+pip install requests
+pip install httplib2
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb forum'
