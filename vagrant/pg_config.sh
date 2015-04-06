b8012f33c86b0d19fc4c2b972af092e88d00978f
Philip Mallory <philip@udacity.com>
2015-04-06 14:32:01 -0700

    Add a login alert to explain how to access Vagrant's shared directory

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -1,4 +1,3 @@
-
 apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
@@ -11,3 +10,13 @@ su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb forum'
 su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
+
+alertAlreadyAdded=$(grep -c "vagrantTip.txt" /home/vagrant/.bashrc)
+if [ $alertAlreadyAdded -eq 0 ]
+then
+    echo -e "\n# Print instructions to access vagrant shared directory\ncat /vagrant/.vagrantTip.txt\n" >> /home/vagrant/.bashrc
+    echo "Shared folder alert added to .bashrc"
+else
+    echo "Shared folder alert already present"
+fi
+
