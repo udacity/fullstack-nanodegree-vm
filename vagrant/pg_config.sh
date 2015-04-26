f3ede2428313ecba1b2a9a0c4a81182fae1b9add
xvweirong <xvweirong@hotmail.com>
2015-04-26 20:43:03 +0800

    Update pg_config.sh

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -11,6 +11,6 @@ su vagrant -c 'createdb'
 su vagrant -c 'createdb forum'
 su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
 
-vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant([m"
 echo -e $vagrantTip > /etc/motd
 
