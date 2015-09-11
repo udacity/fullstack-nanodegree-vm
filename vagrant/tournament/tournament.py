b118f2991058878b47a9c9d975e83de8ddfbe1cb
Scott Harman <scott@harman.tv>
2015-09-11 16:53:22 +1000

    initial commit
    testing the tournament db create

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,26 +13,37 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    conn = connect()
+    conn.execute("DELETE from matches; DELETE from playerResults;")
+    conn.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    conn.connect()
+    conn.execute("DELETE from players;")
+    conn.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn.connect()
+    conn.execute("SELECT count(*) from players;")
+    playerCount = conn.fetchone()
+    conn.close()
+    return playerCount
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-
+    conn.connect()
+    conn.execute("INSERT INTO players (player_name) VALUES ($s)", (name))
+    conn.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -56,16 +67,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -74,4 +85,7 @@ def swissPairings():
         name2: the second player's name
     """
 
-
+def close():
+    """Explicitly closes the database connection.
+    """
+    return psycopg2.close()
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,37 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE tournament;
+CREATE DATABASE tournament;
+\c tournament;
 
+-- Match table - records all the matches played by playerStandings
+-- We will use this to record the matches played, and which user was the winner
+CREATE TABLE matches (
+    match_id serial PRIMARY KEY,
+    winner_id int references players (player_id),
+    loser_id  int references players (player_id),
+    round 
+);
+-- Players table - this records the players and details about them.
+CREATE TABLE players (
+    player_id serial PRIMARY KEY,
+    player_name varchar(128)
+);
+
+-- Records the stats about individual player statistics
+CREATE TABLE playerStandings (
+    player_id serial PRIMARY KEY,
+    score int,
+    matches int
+);
+
+
+CREATE TABLE playerResults (
+    player_id int references players (player_id),
+    match_id int references matches (match_id),
+
+)
+
+CREATE VIEW playerStandings as
+    SELECT player_id, player_name
