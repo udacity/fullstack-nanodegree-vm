52c13fc1afbb945c862935d3592cae4dda66f25d
Lorenzo Brown <lorenzo@udacity.com>
2015-10-16 16:12:39 -0700

    Update pg_config.sh
    added passlib

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -7,6 +7,7 @@ pip install oauth2client
 pip install requests
 pip install httplib2
 pip install redis
+pip install passlib
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb forum'
