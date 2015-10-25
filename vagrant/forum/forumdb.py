05633e76bb49a8c572b816a226ef56d09942d80e
Population96 <population96@gmail.com>
2015-10-24 21:49:51 -0400

    Add database support to forum Change Vagrant type to precise32

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,10 +2,8 @@
 # Database access functions for the web forum.
 # 
 
-import time
-
-## Database connection
-DB = []
+import psycopg2
+import bleach
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +14,17 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    try:
+        DB = psycopg2.connect("dbname=forum")
+        cur = DB.cursor()
+        query = "SELECT time, content FROM posts ORDER BY time DESC"
+        cur.execute(query)
+        posts = ({'content': str(bleach.clean(row[1])), 'time': str(row[0])}
+                 for row in cur.fetchall())
+        DB.close()
+    except:
+        print "Cannot connect or retrieve..."
+
     return posts
 
 ## Add a post to the database.
@@ -27,5 +34,17 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    try:
+        DB = psycopg2.connect("dbname=forum")
+        cur = DB.cursor()
+        ## Cleanup:
+        cleaned = bleach.clean(content, strip = True)
+        ## Protect against SQL injection!:
+        cur.execute("INSERT INTO posts (content) VALUES (%s)",
+                    (cleaned,))
+        DB.commit()
+        DB.close()
+    except psycopg2.DatabaseError, e:
+        if DB:
+            DB.rollback()
+	print "Error '%s'" % e
