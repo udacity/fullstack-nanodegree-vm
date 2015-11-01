d58df0ef616c22a91d066e468979d82e6997fe4f
Population96 <population96@gmail.com>
2015-11-01 18:04:16 -0500

    Rough SQL creation code added.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,38 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE tournaments
+(
+	TID serial NOT NULL PRIMARY KEY,
+	TDate date DEFAULT GETDATE(),
+	TName varchar(30) NOT NULL,
+	Enroll bool DEFAULT TRUE,
+	Rounds int
+);
 
+CREATE TABLE players
+(
+	PID serial NOT NULL PRIMARY KEY,
+	PName varchar(30)
+);
+
+CREATE TABLE registration
+(
+	RID serial NOT NULL PRIMARY KEY,
+	TID integer FOREIGN KEY REFERENCES tournaments(TID),
+	PID integer FOREIGN KEY REFERENCES players(PID),
+	Wins int,
+	Draws int,
+	Losses int
+);
+
+CREATE TABLE matchups
+(
+	MatchID serial NOT NULL PRIMARY KEY,
+	PlayerID integer FOREIGN KEY REFERENCES players(PID),
+	OpponentID integer FOREIGN KEY REFERENCES players(PID),
+	Wins int,
+	Draws int,
+	Losses int,
+	Rount int
+);
