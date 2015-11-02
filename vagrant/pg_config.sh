e9f2653c0f812d842ce36b2a2205eab17a7baa31
Lorenzo Brown <lorenzo@udacity.com>
2015-11-02 12:49:35 -0800

    Update pg_config.sh
    removed duplicate oauth2client

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -10,7 +10,6 @@ pip install redis
 pip install passlib
 pip install itsdangerous
 pip install flask-httpauth
-pip install oauth2client
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb forum'
