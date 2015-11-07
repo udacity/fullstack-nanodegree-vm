ddbf13feafc78e64c04ad900019d31c9e07b8001
Population96 <population96@gmail.com>
2015-11-07 18:08:17 -0500

    Start coding functions

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,7 +4,7 @@
 #
 
 import psycopg2
-
+import bleach
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -13,15 +13,30 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    DB = connect()
+    c = DB.cursor()
+    query = "DELETE from matches; --"
+    DB.commit()
+    DB.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    DB = connect()
+    c = DB.cursor()
+    query = "DELETE from players; --"
+    DB.commit()
+    DB.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    DB = connect()
+    c = DB.cursor()
+    query = "SELECT count(RID) as COUNT FROM registration GROUP BY RID"
+    count = ({'COUNT': int(row[0])}
+                 for row in cur.fetchall())
+    DB.commit()
+    DB.close()
+    return count
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
@@ -32,7 +47,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-
+    DB = connect()
+    c = DB.cursor()
+    query = "INSERT INTO players(PName) VALUES ('%s')" % (name)
+    DB.commit()
+    DB.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -47,7 +66,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-
+    DB = connect()
+    c = DB.cursor()
+    query = ("SELECT PName, p.PID, r.Wins, r.Matches FROM players(PName)"
+             " as p, registration as r WHERE p.PID = r.PID ORDER BY r.Matches"
+    DB.commit()
+    DB.close()
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,7 +80,11 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    DB = connect()
+    c = DB.cursor()
+    query = "INSERT INTO matches() VALUES ('%s')" % (name)
+    DB.commit()
+    DB.close()
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
