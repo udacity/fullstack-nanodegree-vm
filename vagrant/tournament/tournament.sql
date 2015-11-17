7941a1b73e30732111faa02c16f675d7c2888ef0
ethan <erogers6264@gmail.com>
2015-11-16 19:49:43 -0700

    Set up initial db schema

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,21 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+drop database if exists tournament;
+create database tournament;
 
+\c tournament;
+drop table if exists players;
+create table players(
+	player_id serial primary key,
+	name varchar(50) not null,
+	wins integer default 0, --could be a count of the matches table
+	losses integer default 0 -- same here
+	);
+
+drop table if exists matches;
+create table matches(
+	match_id serial primary key,
+	winner integer references players (player_id),
+	loser integer references players (player_id)
+	);
