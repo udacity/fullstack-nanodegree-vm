97abf35d9f45c5603687200b68bf5248520d49bb
Stanley Calixte <DuCalixte@gmail.com>
2015-11-17 23:12:20 -0500

    update bash options

--- vagrant/catalog/pg_config.sh
+++ vagrant/catalog/pg_config.sh
@@ -1,16 +1,21 @@
-sudo apt-get -qqy update
-sudo apt-get -qqy install npm
-sudo apt-get -qqy install python-virtualenv
-sudo apt-get -qqy install python-sqlalchemy
-sudo apt-get -qqy install python-pip
+# sudo apt-get -qqy update
+# sudo apt-get -qqy install nodejs
+# sudo apt-get -qqy install npm
+# sudo apt-get install nodejs-legacy
+# sudo apt-get -qqy install python-virtualenv
+# sudo apt-get -qqy install python-sqlalchemy
+# sudo apt-get -qqy install python-pip
+# sudo apt-get -qqy install git
+pip install flask==0.10.1
 pip install sqlalchemy
 pip install requests
 pip install werkzeug==0.10.4
 pip install watchdog==0.8.3
-pip install flask==0.10.1
 pip install Flask-Login==0.1.3
 pip install httplib2
 pip install webapp2
 pip install webob
-sudo npm install bower -g
-bower install --save
+pip install oauth2client
+pip install argparse
+# sudo npm install bower -g
+# bower install --save
