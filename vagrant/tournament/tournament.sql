44fe29209dcf01c1d8bc12e31e6989f7c200c75f
ethan <erogers6264@gmail.com>
2015-11-18 16:10:37 -0700

    Convert case for SQL keywords

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,21 +6,21 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-drop database if exists tournament;
-create database tournament;
+DROP DATABASE IF EXISTS tournament;
+create DATABASE tournament;
 
-\c tournament;
-drop table if exists players;
-create table players(
-	player_id serial primary key,
-	name varchar(50) not null,
-	wins integer default 0,
-	losses integer default 0
+\C tournament;
+DROP TABLE IF EXISTS players;
+CREATE TABLE players(
+	player_id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	wins INTEGER default 0,
+	losses INTEGER default 0
 	);
 
-drop table if exists matches;
-create table matches(
-	match_id serial primary key,
-	winner integer references players (player_id),
-	loser integer references players (player_id)
+DROP TABLE IF EXISTS matches;
+CREATE TABLE matches(
+	match_id SERIAL PRIMARY KEY,
+	winner INTEGER REFERENCES players (player_id),
+	loser INTEGER REFERENCES players (player_id)
 	);
