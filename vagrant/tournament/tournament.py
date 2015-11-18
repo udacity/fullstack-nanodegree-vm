d8c6faecba6da1ee1f42b0091a1d51a703e72860
ethan <erogers6264@gmail.com>
2015-11-18 15:30:06 -0700

    Make code comply with PEP8 online check

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -21,6 +21,7 @@ def deleteMatches():
     db.commit()
     db.close()
 
+
 def deletePlayers():
     """Remove all the player records from the database."""
     db = connect()
@@ -30,6 +31,7 @@ def deletePlayers():
 
     db.commit()
     db.close()
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
@@ -42,12 +44,13 @@ def countPlayers():
     db.close()
     return int(count[0])
 
+
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -59,10 +62,11 @@ def registerPlayer(name):
     db.commit()
     db.close()
 
+
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a 
+    The first entry in the list should be the player in first place, or a
     player tied for first place if there is currently a tie.
 
     Returns:
@@ -82,6 +86,7 @@ def playerStandings():
     db.close()
     return standings
 
+
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
 
@@ -93,25 +98,25 @@ def reportMatch(winner, loser):
     c = db.cursor()
 
     c.execute("insert into matches (winner, loser) values (%s, %s)",
-                (winner, loser,))
+              (winner, loser,))
     c.execute("""UPDATE players
                  SET wins=wins + 1
                  WHERE player_id=%s; """, (winner,))
     c.execute("""UPDATE players
                  SET losses=losses + 1
                  WHERE player_id=%s; """, (loser,))
-
     db.commit()
     db.close()
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -122,11 +127,10 @@ def swissPairings():
     db = connect()
     c = db.cursor()
 
-
     standings = playerStandings()
     idnamepairs = [(row[0], row[1]) for row in standings]
     pairs = []
-    
+
     i = 0
     while i < len(idnamepairs):
         pairs.append(idnamepairs[i] + idnamepairs[i+1])
@@ -134,4 +138,3 @@ def swissPairings():
 
     db.close()
     return pairs
-
