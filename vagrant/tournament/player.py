18c8f6bf5043a5f41983f1008b1584437dc8ec94
Population96 <population96@gmail.com>
2015-12-18 18:01:27 -0500

    Cleaned up code.  Added sort for Players by record.

--- vagrant/tournament/player.py
+++ vagrant/tournament/player.py
@@ -4,18 +4,20 @@
 #
 
 class Player:
-    def __init__(self, id, name, wins, draws, losses, matches, record):
+    def __init__(self, id, name, wins, draws, losses, matches):
         self.id = id
         self.name = name
         self.wins = wins
         self.draws = draws
         self.losses = losses
         self.matches = matches
-        self.record = record
+        self.record = self.__match_record()
 
     def __repr__(self):
         return repr((self.id, self.name, self.wins, self.draws,
                      self.matches, self.record))
 
     def match_record(self):
-        return 0
+        return (self.wins + self.draws) / float(self.matches)
+
+    __match_record = match_record
