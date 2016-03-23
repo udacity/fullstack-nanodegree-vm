d8e2339a666c025117ff09854982d6cb6a6dbfc5
playwriter <playwriter@users.noreply.github.com>
2016-03-24 02:26:22 +0300

    Update tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,26 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-
-
+--
+-- Clean uo any previous tournament databases.
+DROP DATABASE IF EXISTS tournament;
+--
+-- Create database.
+CREATE DATABASE tournament;
+--
+-- Connect to the DB before creating tables.
+\c tournament;
+--
+-- Create table for payers.
+CREATE TABLE players(id serial primary key, name text);
+--
+-- Create table for games.
+CREATE TABLE matches(game_id serial primary key, winner integer references players(id), loser integer references players(id), PRIMARY KEY9winner, loser));
+--
+-- Create a view that counts total matches per winner.
+CREATE OR REPLACE VIEW games-view AS
+SELECT players.id;
+COUNT(matches.*)AS games
+From players LEFT JOIN matches
+ON players.id = matches.winner OR players.id = matches.loser
+GROUP BY players.id;
