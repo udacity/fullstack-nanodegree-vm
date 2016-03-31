00d915fa2d7b2c555c89a7da0d79c2e324ef4631
Eric Whitfield <Eric Whitfield>
2016-03-31 18:36:25 -0500

    feat: Connect database to the python file
    Database is connected.  Ability to add posts to the database works.
    Posts in the database do not show up in the webpage.
    
    look for why the posts are not displaying correctly.

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,11 @@
 #
 # Database access functions for the web forum.
 # 
-
-import time
+import psycopg2
+from datetime import datetime
 
 ## Database connection
-DB = []
+#DB = psycopg2.connect("dbname=forum")
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +16,22 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum")
+
+    cursor = DB.cursor()
+
+    cursor.execute("select content, time from posts order by time desc")
+    #posts = cursor.fetchall() 
+
+    print cursor.fetchall()
+
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in cursor.fetchall()]
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+
+    print posts
+
+    DB.close()
+
     return posts
 
 ## Add a post to the database.
@@ -27,5 +41,19 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    #t = time.strftime('%c', time.localtime())
+    #DB.append((t, content))
+
+    DB = psycopg2.connect("dbname=forum")
+
+    ctime = datetime.now()
+    cursor = DB.cursor()
+
+    #exstring = "insert into posts values (\'%s\', %s)" % (content, ctime) 
+    exstring = "insert into posts values (\'%s\')" % (content)
+    cursor.execute(exstring)
+
+    DB.commit()
+    DB.close()
+
+#DB.close()
