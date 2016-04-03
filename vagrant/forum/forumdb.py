7c5fad1b9a392735bf2d2b0f26de8b406230dddb
Eric Whitfield <Eric Whitfield>
2016-04-03 13:38:39 -0500

    Feat: Connect DB object to actual database
    GetAllPosts returns a dict from the contents in the database.  Webpage
    correctly displays the post history because of this fix.

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,6 +2,7 @@
 # Database access functions for the web forum.
 # 
 import psycopg2
+import psycopg2.extras
 from datetime import datetime
 
 ## Database connection
@@ -18,17 +19,10 @@ def GetAllPosts():
     '''
     DB = psycopg2.connect("dbname=forum")
 
-    cursor = DB.cursor()
+    cursor = DB.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     cursor.execute("select content, time from posts order by time desc")
-    #posts = cursor.fetchall() 
-
-    print cursor.fetchall()
-
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in cursor.fetchall()]
-    #posts.sort(key=lambda row: row['time'], reverse=True)
-
-    print posts
+    posts = cursor.fetchall() 
 
     DB.close()
 
@@ -41,15 +35,10 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    #t = time.strftime('%c', time.localtime())
-    #DB.append((t, content))
-
     DB = psycopg2.connect("dbname=forum")
 
-    ctime = datetime.now()
     cursor = DB.cursor()
 
-    #exstring = "insert into posts values (\'%s\', %s)" % (content, ctime) 
     exstring = "insert into posts values (\'%s\')" % (content)
     cursor.execute(exstring)
 
