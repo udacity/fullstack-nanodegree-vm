b448f459416b1f01dfde6e44e0861d823d3cea25
Abhishek Kumar Singh <abhishek291093@gmail.com>
2016-04-23 16:46:40 +0530

    files updated and working properlly

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,21 +3,22 @@
 # 
 
 import time
+import psycopg2
 
 ## Database connection
 DB = []
 
 ## Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
+    DB = psycopg2.connect("dbname = forum")
+    c = DB.cursor()
+    c.execute(
+      "SELECT time, content FROM posts ORDER BY time DESC"
+      )
+    posts = ({'content': str(row[1]), 'time': str(row[0])}
+              for row in c.fetchall())
+    DB.close()
 
-    Returns:
-      A list of dictionaries, where each dictionary has a 'content' key
-      pointing to the post content, and 'time' key pointing to the time
-      it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
     return posts
 
 ## Add a post to the database.
@@ -27,5 +28,9 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname = forum")
+    c = DB.cursor()
+    c.execute("INSERT INTO posts (content) VALUES (%s)",
+      (content,))
+    DB.commit()
+    DB.close()
