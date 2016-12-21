5364c959703cb723b1fa0597b9a2df8d3c6f1328
pauldcastillo <paul.d.castillo@gmail.com>
2016-12-21 03:07:39 -0800

    Remove debug drops.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,12 +6,8 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-DROP DATABASE  IF EXISTS tournament;
-
 CREATE DATABASE tournament;
 \c tournament
-DROP TABLE IF EXISTS players CASCADE;
-DROP TABLE IF EXISTS matches;
 CREATE TABLE players (
     id serial UNIQUE primary key,
     name text
