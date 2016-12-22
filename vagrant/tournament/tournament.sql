40d2b07c93f1a3cd556a27da4667c45a175320c0
pauldcastillo <paul.d.castillo@gmail.com>
2016-12-22 01:27:43 -0800

    Also drop tables. Fix create views by adding ;

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -10,6 +10,8 @@ DROP DATABASE IF EXISTS tournament;
 
 CREATE DATABASE tournament;
 \c tournament
+DROP TABLE IF EXISTS players CASCADE;
+DROP TABLE IF EXISTS matches;
 CREATE TABLE players (
     id serial UNIQUE primary key,
     name text
@@ -18,15 +20,15 @@ CREATE TABLE matches (
     id serial UNIQUE primary key,
     winner integer REFERENCES players(id),
     loser integer REFERENCES players(id)
-)
+);
 CREATE VIEW player_wins AS
 SELECT players.id as id, players.name as name, count(matches.winner) as wins
 FROM players LEFT JOIN matches
 ON players.id = matches.winner
-GROUP BY players.id
+GROUP BY players.id;
 
 CREATE VIEW player_matches AS
 SELECT players.id as id, count(matches.winner + matches.loser) as matches
 FROM players LEFT JOIN matches
 ON players.id = matches.winner or players.id = matches.loser
-GROUP BY players.id
+GROUP BY players.id;
