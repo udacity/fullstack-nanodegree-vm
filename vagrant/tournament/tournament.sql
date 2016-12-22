c7f303245a6806b0d66ac787f12c44ff5bf2a1ec
pauldcastillo <paul.d.castillo@gmail.com>
2016-12-22 00:55:53 -0800

    Drop database if it exists.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,6 +6,8 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
+
 CREATE DATABASE tournament;
 \c tournament
 CREATE TABLE players (
