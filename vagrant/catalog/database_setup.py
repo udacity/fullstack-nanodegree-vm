e6d77969c796dcd6ded33c7c0e7c1ec3e56cd97d
jordan7272 <jordan7272@yahoo.com>
2016-12-27 01:03:47 -0800

    large commit

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Sport(Base):
+    __tablename__ = 'sport'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class SportItem(Base):
+    __tablename__ = 'sport_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    sport_id = Column(Integer, ForeignKey('sport.id'))
+    sport = relationship(Sport)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
+
+
+engine = create_engine('sqlite:///sports.db')
+
+
+Base.metadata.create_all(engine)
