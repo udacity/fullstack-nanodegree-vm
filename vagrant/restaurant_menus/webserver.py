984343d6f24f2c948aaf24fa89ab0da6904d57df
pauldcastillo <paul.d.castillo@gmail.com>
2017-01-08 22:00:31 -0800

    Add html_wrapper and send_get_response

--- vagrant/restaurant_menus/webserver.py
+++ vagrant/restaurant_menus/webserver.py
@@ -1,44 +1,82 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 import cgi
+
+
+# Create database
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
+
+# Create database connector
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+def html_wrapper(html):
+    """
+    Returns a string wrapper with <html><body></body></html>.
+
+    >>> output = html_wrapper("hello")
+    >>> print output
+    >>> <html><body>hello</body></html>
+    """
+    output = "<html><body>"
+    output += html
+    output += "</body></html>"
+    return output
+
+class RestuarantsHandler(object):
+    """Class for handling actions related to restaurants."""
+
+    @classmethod
+    def get_restaurants(cls):
+        """Returns a list of all the restuarants."""
+        return session.query(Restaurant).all()
 
 
 class webserverHandler(BaseHTTPRequestHandler):
     """docstring for webserverHandler"""
+    def send_get_response(self, output):
+        """
+        Sends a 200 response and text/html header, then sends and
+        prints the given output.
+
+        >>> send_get_response("<html><body>hello</body></html>")
+        >>> <html><body>hello</body></html>
+        """
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        html_output = html_wrapper(output)
+        self.wfile.write(html_output)
+        print html_output
+
     def do_GET(self):
         try:
             if self.path.endswith("/hello"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-
+                # Handler for hello page
                 output = ""
-                output += "<html><body>"
                 output += "Hello!"
                 output += ("<form method='POST' enctype='multipart/form-data' "
                            "action='/hello'><h2>What would you like me to say?"
                            "</h2><input name='message' type='text' ><input "
                            "type='submit' value='Submit'> </form>")
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
+                send_get_response(output)
                 return
-            if self.path.endswith("/hola"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
 
+            if self.path.endswith("/hola"):
+                # Handler for hola page
                 output = ""
-                output += "<html><body>"
                 output += ("&#161Hola! <a href = '/hello' > Back "
                            "to Hello </a>")
                 output += ("<form method='POST' enctype='multipart/form-data' "
                            "action='/hello'><h2>What would you like me to say?"
                            "</h2><input name='message' type='text' ><input "
                            "type='submit' value='Submit'> </form>")
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
+                send_get_response(output)
                 return
+
 
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
