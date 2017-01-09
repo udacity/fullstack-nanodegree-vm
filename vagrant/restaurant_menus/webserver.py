346578da54d2b82b05b08b58efb76831a96278b3
pauldcastillo <paul.d.castillo@gmail.com>
2017-01-08 21:09:14 -0800

    Add submitting a message.

--- vagrant/restaurant_menus/webserver.py
+++ vagrant/restaurant_menus/webserver.py
@@ -12,7 +12,13 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
 
                 output = ""
-                output += "<html><body>Hello!</body></html>"
+                output += "<html><body>"
+                output += "Hello!"
+                output += ("<form method='POST' enctype='multipart/form-data' "
+                           "action='/hello'><h2>What would you like me to say?"
+                           "</h2><input name='message' type='text' ><input "
+                           "type='submit' value='Submit'> </form>")
+                output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
@@ -22,13 +28,49 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
 
                 output = ""
-                output += "<html><body>&#161Hola! <a href = '/hello' > Back to Hello </a></body></html>"
+                output += "<html><body>"
+                output += ("&#161Hola! <a href = '/hello' > Back "
+                           "to Hello </a>")
+                output += ("<form method='POST' enctype='multipart/form-data' "
+                           "action='/hello'><h2>What would you like me to say?"
+                           "</h2><input name='message' type='text' ><input "
+                           "type='submit' value='Submit'> </form>")
+                output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
 
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.end_headers()
+
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+
+            output = ""
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+
+            output += ("<form method='POST' enctype='multipart/form-data' "
+                       "action='/hello'><h2>What would you like me to say?"
+                       "</h2><input name='message' type='text' ><input "
+                       "type='submit' value='Submit'> </form>")
+            
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+
+
+        except:
+            pass
         
 
 def main():
