7faef90c2157e64611618cbb9e3b5e147f3f558c
jordan7272 <jordan7272@yahoo.com>
2017-01-16 21:03:54 -0800

    Update project.py

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -439,4 +439,4 @@ def disconnect():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='35.164.182.61', port=2200)
