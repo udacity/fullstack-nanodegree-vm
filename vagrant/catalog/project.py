15235e3a0d405cfd62cc1092ad34f99820539d2b
Alonzo Machiraju <machthree@gmail.com>
2017-01-25 12:40:24 -0800

    Feat: Basic server setup code, catalog.json added
    Though no server functionality has been implemented yet, and neither
    have the required libraries been added, the basics are currently being
    implemented.

---
+++ vagrant/catalog/project.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+
+
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem, User
+
+from flask import session as login_session
+import random
+import string
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
+APPLICATION_NAME = "Item Catalog"
+
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
