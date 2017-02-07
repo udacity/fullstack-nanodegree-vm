395458ce88c345e00f9d28af1e0199a5c6e2f88c
Karl Krueger <karl@udacity.com>
2017-02-07 15:25:32 -0800

    Fix broken solution.

--- vagrant/forum/solution/forumdb_stepone.py
+++ vagrant/forum/solution/forumdb_stepone.py
@@ -18,7 +18,7 @@ def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
-  c.execute("insert into posts values (%s)" % content)  # Almost but not quite.
+  c.execute("insert into posts values ('%s')" % content) # Almost but not quite.
   db.commit()
   db.close()
 
