d43f0bfb15a661f9fdaedc74d5c54fc1a857d1c4
Karl Krueger <karl@udacity.com>
2017-02-06 16:17:44 -0800

    Remove duplicate app.run

--- vagrant/forum/forum.py
+++ vagrant/forum/forum.py
@@ -61,5 +61,4 @@ def post():
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000)
-  app.run(debug=True)
 
--- vagrant/forum/solution/forum.py
+++ vagrant/forum/solution/forum.py
@@ -61,5 +61,3 @@ def post():
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000)
-  app.run(debug=True)
-
