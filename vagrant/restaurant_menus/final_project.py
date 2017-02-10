ff521217c4318239e2da0105caad22b9e8e5a280
pauldcastillo <paul.d.castillo@gmail.com>
2017-02-09 21:39:18 -0800

    Create html for google login.

--- vagrant/restaurant_menus/final_project.py
+++ vagrant/restaurant_menus/final_project.py
@@ -45,7 +45,7 @@ def show_login():
     state = ''.join(random.choice(
         string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    return render_template('login.html')
 
 @app.route('/')
 @app.route('/restaurants/')
