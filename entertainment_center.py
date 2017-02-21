7642e00c5959e72ee6c8d4dbe8881bc7996d52de
Kavitha Rayanki <rayanki.kavitha@gmail.com>
2017-02-20 22:55:17 -0800

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,22 +1,25 @@
 import media
 import fresh_tomatoes
 
-# Create a list of movies
-
+# create instances of movie and add them to a list
 toy_story = media.Movie("Toy Story",
+                        "A story about a boy and his toys that comes to a life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc"
                         )
 
 avatar = media.Movie("Avatar",
-                        "https://upload.wikimedia.org/wikipedia/en/5/5c/Avatar_picture.jpg",
-                        "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+                     "Story about a Marine who visits another planet",
+                      "https://upload.wikimedia.org/wikipedia/en/5/5c/Avatar_picture.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY"
                         )
 moana = media.Movie("Moana",
+                    "Story about a brave Hawaiian princess",
                      "https://upload.wikimedia.org/wikipedia/en/2/26/Moana_Teaser_Poster.jpg",
                     "https://www.youtube.com/watch?v=LKFuXETZUsI"
                      )
-
 movies=[toy_story, avatar, moana]
 
+# This function call uses list of movie instances as input to generate an HTML file
+# and open it in the browser.
 fresh_tomatoes.open_movies_page(movies)
