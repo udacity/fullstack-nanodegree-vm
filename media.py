becd428706bc420a59c88772a904ec8a99e75e43
Kavitha Rayanki <rayanki.kavitha@gmail.com>
2017-02-20 22:56:28 -0800

    Update media.py

--- media.py
+++ media.py
@@ -1,11 +1,15 @@
 import webbrowser
 
 class Movie():
-    def __init__(self, movie_name, movie_icon, movie_trl):
+    """Class for movie objects and methods """
+
+    def __init__(self, movie_name, storyline, movie_icon, movie_trl):
+        """This method initalizes a movie object with title, story line ,icon and youtube trailer """
         self.title = movie_name
-        #self.story_line = story_line
+        self.storyline = storyline
         self.poster_image_url = movie_icon
         self.trailer_youtube_url = movie_trl
 
     def show_trailer(self):
+        """ opens up youtube trailer on the movie object"""
         webbrowser.open(self.trailer_youtube_url)
