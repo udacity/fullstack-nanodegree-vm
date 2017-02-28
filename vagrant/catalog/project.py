928afdebbd7537224022cfcb7276a80afae53591
Alonzo Machiraju <machthree@gmail.com>
2017-02-27 22:07:23 -0800

    FIXED: Shows items in categories

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -109,7 +109,7 @@ def newItem(category_id):
 
 # Show item information
 @app.route('/catalog/<int:category_id>/items/<int:id>')
-def showItem(id):
+def showItem(category_id, id):
     item = session.query(Item).filter_by(id = id)
     return render_template('item.html', item=item)
 
