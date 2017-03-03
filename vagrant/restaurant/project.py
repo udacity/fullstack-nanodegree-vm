1c9c70ee261d90ecd0c9a35d04e6653eb6c3ae23
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-03-03 08:53:51 +0600

    Add the database with restaurants to flask app

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,9 +1,36 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem, engine
+
+
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/')
 @app.route('/hello')
-def HelloWorld():
-    return "hello world"
+def restaurantMenu():
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+
+    output = ''
+
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br></br></br>'
+
+    return output
+
 
 if __name__ == '__main__':
     app.debug = True
