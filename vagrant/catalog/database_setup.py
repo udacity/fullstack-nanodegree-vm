9563ee124d4cda239df8c7a00803874b9d0e4f2f
Alonzo Machiraju <machthree@gmail.com>
2017-03-02 17:15:24 -0800

    Minor edits + failed attempt to make Item JSON serializable

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -58,7 +58,6 @@ class Item(Base):
            'name'         : self.name,
            'description'         : self.description,
            'id'         : self.id,
-           'catalog_id'    : self.catalog_id,
        }
 
 
