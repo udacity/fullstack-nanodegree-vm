86fc9470ddc7bbdb9c39ffd2f456cc26f26b568c
Steven Wooding <steve@stevenwooding.com>
2017-03-18 14:39:05 +0000

    Install latest version of Pip
    The Ubuntu distribution version of Pip no longer works (complains about module packaging missing when running pip).
    The solution is to install the very latest version of Pip instead. This also fixes issues with the dependencies for the
    Bleach package.

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -1,7 +1,8 @@
 apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
-apt-get -qqy install python-pip
+wget https://bootstrap.pypa.io/get-pip.py
+python ./get-pip.py
 pip install bleach
 pip install oauth2client
 pip install requests
