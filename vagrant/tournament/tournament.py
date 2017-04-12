9c2e64998e471db7845548fdd2c104f667e7d6e6
Waseem Ahmed <alphawaseem@gmail.com>
2017-04-12 16:49:06 +0530

    working 80%

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,25 +13,47 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("delete from matches;")
+    db.commit()
+    db.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("delete from players;")
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("select * from num_players;")
+    count = cur.fetchall()
+    db.commit()
+    db.close()
+    return int(count[0][0])
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    db = connect()
+    cur = db.cursor()
+    cur.execute("insert into players (name)  values (%s)", (name,))
+    db.commit()
+    db.close()
 
 
 def playerStandings():
@@ -47,6 +69,13 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    cur = db.cursor()
+    cur.execute("select * from player_standing;")
+    res = cur.fetchall()
+    db.commit()
+    db.close()
+    return res
 
 
 def reportMatch(winner, loser):
@@ -56,16 +85,22 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    db = connect()
+    cur = db.cursor()
+    cur.execute("insert into matches (winner,loser) values (%s,%s)",
+                (winner, loser))
+    db.commit()
+    db.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +108,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,51 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+drop database if exists tournament;
+
+create database tournament;
+\c tournament;
+
+
+drop table if exists players;
+create table players (
+    id serial primary key,
+    name text
+);
+
+
+drop table if exists matches;
+create table matches (
+    id serial primary key, 
+    winner int references players(id),
+    loser int references players(id)
+);
+
+
+create view num_players as select count(*) from players;
+create view num_matches as select count(*) from matches;
+
+create view win_total as 
+    select players.id, name,count(winner) as wins from players left join matches on players.id = matches.winner group by players.id;
+
+create view matches_played as 
+    select players.id, name,count(matches.winner) as total_matches from players left join matches on players.id in (matches.winner,matches.loser) group by players.id;
+
+
+create view player_standing as
+    select win_total.id,win_total.name,win_total.wins,matches_played.total_matches from matches_played join win_total on matches_played.id = win_total.id order by wins desc;
+
+
+insert into players (name) values ('Waseem');
+insert into players (name) values ('Wasim');
+insert into players (name) values ('Ahmed');
+insert into players (name) values ('Mohammad');
+
+insert into matches (winner,loser) values (1,2);
+insert into matches (winner,loser) values (1,2);
+insert into matches (winner,loser) values (3,2);
+insert into matches (winner,loser) values (2,1);
+insert into matches (winner,loser) values (3,1);
+
+
 
