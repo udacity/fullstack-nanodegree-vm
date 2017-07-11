d0a98601e82a279dd786c7819f09c23e5466f7c0
davidandrus <davidandrus.com@gmail.com>
2017-07-11 11:43:19 -0700

    adding debug mode for live reloading, adding style.css, some custom styling and some more placeholder for main page

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 @app.route('/')
 def main():
     return render_template('main.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
+    # @TODO - remove debug mode
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
