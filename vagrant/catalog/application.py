38179f35cd2d372766d3753e91f4cb522f597bc2
davidandrus <davidandrus.com@gmail.com>
2017-07-11 10:05:08 -0700

    getting basic template/layout working

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello World"
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000)
