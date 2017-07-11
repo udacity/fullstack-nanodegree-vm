1ed285a75b8e2364017ab18352df5a754c401b5b
davidandrus <davidandrus.com@gmail.com>
2017-07-11 13:35:11 -0700

    adding category and item pages

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -7,6 +7,18 @@ app = Flask(__name__, static_url_path="/static")
 def main():
     return render_template('main.html')
 
+# @TODO - make sure accepts a category slug 
+@app.route('/category')
+def category():
+    return render_template('category.html')
+
+# @TODO - make sure accepts a item slug 
+@app.route('/item')
+def item():
+    return render_template('item.html')
+
+
+
 if __name__ == '__main__':
     # @TODO - remove debug mode
     app.run(host='0.0.0.0', port=8000, debug=True)
