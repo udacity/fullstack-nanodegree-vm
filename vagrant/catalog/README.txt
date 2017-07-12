caee49e45d395bdff711854f8c6921c4f20ce571
davidandrus <davidandrus.com@gmail.com>
2017-07-12 08:21:16 -0700

    adding item class and doing conditional setup of database

--- vagrant/catalog/README.txt
+++ vagrant/catalog/README.txt
@@ -3,4 +3,6 @@ Place your catalog project in this directory.
 #TODO - probably add a new db user instead of using vagrant
 run `vagrant ssh`
 run `psql`
-run ALTER USER vagrant WITH PASSWORD 'vagrant';
+run `ALTER USER vagrant WITH PASSWORD 'vagrant'`;
+run `cd /vagrant/catalog`
+run `python setup_db.py`
--- vagrant/catalog/setup_db.py
+++ vagrant/catalog/setup_db.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.reflection import Inspector
 
 categories = [
     "Alternative",
@@ -34,29 +35,43 @@ Base = declarative_base()
 
 # @TODO - disable extra logging
 engine = create_engine("postgresql://vagrant:vagrant@localhost/catalog", echo=True)
+inspector = Inspector.from_engine(engine)
 
 # create Session class
 Session = sessionmaker(bind=engine)
 
 class Category(Base):
-    __tablename__ = 'categories'
+    __tablename__ = "categories"
     id = Column(Integer, primary_key=True)
-    
-    # @TODO - make unique
     name = Column(String)
 
     def __repr__(self):
         return "<Category(name='%s')>" % ( self.name )
 
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return "<Item(name='%s', category_id='%s')>" % ( self.name, self.category_id )
+
 
 # create tables if they don't exist yet
-Base.metadata.create_all(engine) 
+if "categories" in inspector.get_table_names():
+    Base.metadata.create_all(engine)
+    session = Session()
+
+    for category in categories:
+        new_category = Category(name=category)
+        session.add(new_category)
+    
+    session.commit()
+    session.close()
 
 # create Session instance
-session = Session()
 
-for category in categories:
-    new_category = Category(name=category)
-    session.add(new_category)
 
-session.commit()
+
+
