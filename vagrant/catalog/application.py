445f13ede4972ff78f0628a5f7ef8ee8386bf9f7
davidandrus <davidandrus.com@gmail.com>
2017-07-15 10:27:16 -0700

    getting newly added items displaying on the main page

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request, redirect
 from db import Category, Item, DBSession
+from sqlalchemy import desc
 
 app = Flask(__name__, static_url_path="/static")
+session = DBSession()
 
 def get_all_categories():
-    session = DBSession()
     categories = session.query(Category).order_by(Category.name).all()
-    session.close()
     return categories
 
+def get_recent_items():
+    items = session.query(Item).order_by(desc(Item.created_at)).all()
+    return items
 
 @app.route('/')
 def main():
-    return render_template('main.html', categories=get_all_categories())
+    return render_template('main.html', categories=get_all_categories(), items=get_recent_items())
 
 # @TODO - make sure accepts a category slug 
 @app.route('/category')
@@ -32,6 +35,8 @@ def create_item():
         name = request.form['name']
         description = request.form['description']
         category_id = request.form['category']
+
+        # @TOOD - return errors if name or category is empty
 
         session = DBSession()
         new_item = Item(name=name, description=description, category_id=category_id)
--- vagrant/catalog/db.py
+++ vagrant/catalog/db.py
@@ -1,7 +1,8 @@
+import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
@@ -23,9 +24,11 @@ class Item(Base):
     __tablename__ = 'items'
     id = Column(Integer, primary_key=True)
     category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
+    category = relationship('Category', backref="categories")
     name = Column(String, nullable=False)
     # @TODO - add description column
     description = Column(Text);
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
 
     def __repr__(self):
         return "<Item(name='%s', category_id='%s')>" % ( self.name, self.category_id )
