24ef0283cc0f58335b833b9197e8a13dc5bf7d41
UGEH <smugeh@gmail.com>
2017-10-18 17:11:05 +0100

    Restaurants names from db

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -1,66 +1,34 @@
 #!/usr/bin/env python2.7
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import cgi
+# import CRUD Operations from Lesson 1
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_initial import Base, Restaurant, MenuItem
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 class webServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-            if self.path == "/hello":
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = ""
-                output += "<html><body>"
-                output += "<h1>Hello!</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you 
-                like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += "</body></html>"
-                self.wfile.write(output.encode())
-                print(output)
-                return
-
-            if self.path == "/hola":
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = ""
-                output += "<html><body>"
-                output += "<h1>&#161 Hola !</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you 
-                like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += "</body></html>"
-                self.wfile.write(output.encode())
-                print(output)
-                return
-
-            else:
-                self.send_error(404, 'File Not Found: %s' % self.path)
-
-    def do_POST(self):
-        try:
-            self.send_response(301)
+        if self.path == "/restaurants":
+            self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+            restaurants = session.query(Restaurant).all()
             output = ""
             output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'>
-                            <h2>What would you like me to say?</h2>
-                            <input name="message" type="text" >
-                            <input type="submit" value="Submit"> 
-                        </form> '''
+            for restaurant in restaurants:
+                output += restaurant.name
+                output += "<br><br><br>"
+
             output += "</body></html>"
             self.wfile.write(output.encode())
-            print(output)
-        except:
-            pass
+
+        else:
+            self.send_error(404, 'File Not Found: %s' % self.path)
 
 
 def main():
