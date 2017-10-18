550e405a3797b109a127afbf48a0b7b7ab7e2ec2
UGEH <smugeh@gmail.com>
2017-10-18 08:23:53 +0100

    Added a file for changes to the database

---
+++ vagrant/database_edit.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_initial import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+veggieBurgers = session.query(MenuItem).filter_by(name='Veggie Burger')
+for veggieBurger in veggieBurgers:
+    print(veggieBurger.id)
+    print(veggieBurger.price)
+    print(veggieBurger.restaurant.name)
+    print("\n")
+
+UrbanVeggieBurger = session.query(MenuItem).filter_by(id=8).one()
+UrbanVeggieBurger.price = '$2.99'
+session.add(UrbanVeggieBurger)
+session.commit()
+print(UrbanVeggieBurger.price)
+
+
+spinach = session.query(MenuItem).filter_by(name='Spinach Ice Cream').one()
+print(spinach.id)
+print(spinach.price)
+print(spinach.restaurant.name)
+session.delete(spinach)
+session.commit()
