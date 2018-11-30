1c55233af7a6156307b99b9970e33e05fa5874fb
rafasz <43715742+rafasz@users.noreply.github.com>
2018-11-30 00:52:14 -0200

    Adjustments to README.md

--- vagrant/log_analysis/README.md
+++ vagrant/log_analysis/README.md
@@ -1,18 +1,51 @@
-The 
+Logs Analysis
 
+The project goal is to take a database containing access logs of a news web page and answer these questions about them:
 
-** The view below was created to add a column called article_name to log information in order to simplify the join to the table articles **
+1 - What are the most popular three articles of all time?
+2 - Who are the most popular article authors of all time?
+3 - On which days did more than 1% of requests lead to errors?
+
+Prerequisites
+
+This project was developed using python programming language and PostgreSQL database. These are the recommended versions:
+
+PostgreSQL 9.5.14
+Python 3.5.2 / Bleach 3.0.2 / Psycopg2 2.7.6.1
+Python 2.7.12 / Bleach 3.0.2 / Psycopg2 2.7.6.1
+
+Database setup
+
+Steps required to setup database. 
+
+1- Download database from https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+2- Extract file newsdata.sql
+3- Import database
+    vagrant@vagrant:/vagrant/log_analysis$ psql -d news -f newsdata.sql
+
+Views creation
+
+View created to add a column called article_name to log information in order to simplify the join to the table articles
 
 news=> create view log_article as select *,substring(path,10) as Article_Name from log;
 
 
-** The next view was created to consolidate information about logs, articles and authors into a single place **
+View created to consolidate information about logs, articles and authors into a single place
 
 news=> create view complete_log as select path,ip,method,status,la.time,la.id,article_name,title,a.name from log_article la left join articles on la.article_name = articles.slug left join authors as a on a.id = articles.author;
 
-** The two following views were designed to make the calculation of status error codes percentage easier **
+
+Views created to make the calculation of status error codes percentage easier
+
 Total views per day
 news=> create view total_views_by_day as select count(*) as views, time::date as day from complete_log group by day;
 
 Daily access by status
 news=> create view views_by_status as select count(*) as views, time::date as day, status from complete_log group by day,status order by day desc;
+
+
+How to run the software
+
+From the folder logs_analysis, run reports.py
+
+vagrant@vagrant:/vagrant/log_analysis$ ./reports.py
--- vagrant/log_analysis/reports.py
+++ vagrant/log_analysis/reports.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import psycopg2
@@ -6,51 +6,15 @@ import bleach
 
 DBNAME = "news"
 
-
-def get_errors(percentage):
-    conn = None
-    try:
-        conn = psycopg2.connect(database=DBNAME)
-        cur = conn.cursor()
-        print('Em quais dias mais de 1% das requisicoes'
-              ' resultaram em erros')
-
-        cur.execute("select vd.views as total, vs.views, vs.day from "
-                    "views_by_status as vs, total_views_by_day as vd "
-                    "where vs.day = vd.day and vs.status != '200 OK' "
-                    "and (vs.views*100/vd.views) > %s",
-                    (bleach.clean(str(percentage)), ))
-
-        # display the PostgreSQL database server version
-        errors = cur.fetchall()
-
-        for property in errors:
-            requests = float(property[0])
-            num_of_errors = float(property[1])
-            errors_percentage = num_of_errors * 100 / requests
-
-            print ('{0:%B} {0:%d}, {0:%Y} - {1}% errors'
-                   .format(property[2], round(errors_percentage, 2)))
-        print("\n")
-
-        # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-
 def most_popular_articles(quantity):
     conn = None
     try:
         conn = psycopg2.connect(database=DBNAME)
         cur = conn.cursor()
-        print('\nQuais são os três artigos mais populares de todos os tempos?')
-        cur.execute("select title,count(*) as views from complete_log "
-                    "where title != '' group by title order by views "
-                    "desc limit %s", (bleach.clean(str(quantity)), ))
+        print('\nWhat are the most popular three articles of all time?')
+        cur.execute("""select title,count(*) as views from complete_log 
+                    where title != '' group by title order by views 
+                    desc limit %s""", (bleach.clean(str(quantity)), ))
 
         # display the PostgreSQL database server version
         errors = cur.fetchall()
@@ -76,10 +40,9 @@ def most_popular_authors():
     try:
         conn = psycopg2.connect(database=DBNAME)
         cur = conn.cursor()
-        print('Quem são os autores de artigos mais populares '
-              'de todos os tempos?')
-        cur.execute("select name, count(*) as views from complete_log "
-                    "where name != '' group by name order by views desc")
+        print('Who are the most popular article authors of all time?')
+        cur.execute("""select name, count(*) as views from complete_log 
+                    where name != '' group by name order by views desc""")
 
         # display the PostgreSQL database server version
         errors = cur.fetchall()
@@ -89,6 +52,40 @@ def most_popular_authors():
             views = property[1]
             print ('{0} - {1:}'.format(name, views))
         print("\n")
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def get_errors(percentage):
+    conn = None
+    try:
+        conn = psycopg2.connect(database=DBNAME)
+        cur = conn.cursor()
+        print('On which days did more than 1% of requests lead to errors?')
+
+        cur.execute("select vd.views as total, vs.views, vs.day from "
+                    "views_by_status as vs, total_views_by_day as vd "
+                    "where vs.day = vd.day and vs.status != '200 OK' "
+                    "and (vs.views*100/vd.views) > %s",
+                    (bleach.clean(str(percentage)), ))
+
+        # display the PostgreSQL database server version
+        errors = cur.fetchall()
+
+        for property in errors:
+            requests = float(property[0])
+            num_of_errors = float(property[1])
+            errors_percentage = num_of_errors * 100 / requests
+
+            print ('{0:%B} {0:%d}, {0:%Y} - {1}% errors'
+                   .format(property[2], round(errors_percentage, 2)))
+        print("\n")
+
         # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
