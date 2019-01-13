d0efbc0a22ce379bedbd4c9caf02971d69cdd949
Marc Goedemans <mgoedemans2021@Marcs-MacBook-Pro.local>
2019-01-13 12:03:19 -0800

    feat: add mappers to database_setup.py

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,8 +10,19 @@ Base = declarative_base()
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(
+    Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
 
 engine = create_engine(
 'sqlite:///restaurantmenu.db')
