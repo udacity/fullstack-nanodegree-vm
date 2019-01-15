de7011f273bcd98f0d897ab09fe156330003a5ac
Marc Goedemans <mgoedemans2021@Marcs-MacBook-Pro.local>
2019-01-15 13:51:15 -0800

    feat: adjust webserver.py file

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -1,31 +1,30 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-class webserverHandler(BaseHTTPRequestHandler):
-    def do_Get(self):
-        try:
-            if self.path.endswith("/hello"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
 
-                output = ""
-                output += "<html><body><Hello!</body></html>"
-                self.wfile.write(output)
-                print output
-                return
+class WebServerHandler(BaseHTTPRequestHandler):
 
-        except I0Error:
-            self.send_error(404, "File Not Found %s" % self.path)
+    def do_GET(self):
+        if self.path.endswith("/hello"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            message = ""
+            message += "<html><body>Hello!</body></html>"
+            self.wfile.write(message)
+            print message
+            return
+        else:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
 
 def main():
     try:
         port = 8080
-        server = HTTPServer(('',port), webserverHandler)
-        print "Web server running on port %s" % port
+        server = HTTPServer(('', port), WebServerHandler)
+        print "Web Server running on port %s" % port
         server.serve_forever()
-
     except KeyboardInterrupt:
-        print "^C entered, stopping web server..."
+        print " ^C entered, stopping web server...."
         server.socket.close()
 
 if __name__ == '__main__':
