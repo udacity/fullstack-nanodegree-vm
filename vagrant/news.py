0d368baf3e8fcccd2c1d97b41d7502326bf5a0aa
ohayamic <onyebuchi_ohaya@yahoo.co.uk>
2019-02-05 12:07:42 +0100

    This git commit give list of popular authors in the DB

--- vagrant/news.py
+++ vagrant/news.py
@@ -73,3 +73,19 @@ def main():
                     for date, percenterror in get_allpercenterror())
     html = HTML_WRAP % posts
     return html
+
+@app.route('/highesterror', methods=['GET'])
+def highestError():
+    '''Main page of the forum.'''
+    posts = "".join(POST % (date, percenterror)
+                    for date, percenterror in get_morethantwopercent())
+    html = HTML_WRAP % posts
+    return html
+
+@app.route('/popularauthors', methods=['GET'])
+def popularAuthors():
+    '''Main page of the forum.'''
+    posts = "".join(POST1 % (name, sum)
+                    for name, sum in get_popularauthors())
+    html = HTML_WRAP1 % posts
+    return html
