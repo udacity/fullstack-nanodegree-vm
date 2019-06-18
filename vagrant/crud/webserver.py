4e2515cb15a7fc99b82a8f4c46f8724465d34f08
Felipe Olavarría <178.felipe@gmail.com>
2019-06-18 11:35:31 -0400

    Add webserver structure in webserver.py
    - Add webserverHandler in webserver.py

---
+++ vagrant/crud/webserver.py
@@ -0,0 +1,29 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith('/hello'):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = '<html><body>Hello!</body></html>'
+                print(output)
+                response = bytes(output, 'UTF-8')
+                self.wfile.write(response)
+        except IOError:
+            self.send_error(404, 'File Not Found {}'.format(self.path))
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print('WebServer: Server running on port {}'.format(port))
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('WebServer: ^C entered, stopping web server')
+        pass
+
+if __name__ == '__main__':
+    main()
