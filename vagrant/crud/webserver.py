ec6590dc5de9e2ffdb91abdc641b9ff65610bfd2
desolaser <178.felipe@gmail.com>
2019-06-18 12:27:42 -0400

    Add form in all routes in webserver.py
    - Add _set_headers() in webserverHandler

--- vagrant/crud/webserver.py
+++ vagrant/crud/webserver.py
@@ -3,21 +3,38 @@ import cgi
 
 
 class webserverHandler(BaseHTTPRequestHandler):
+    
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
     def do_GET(self):
         try:
             if self.path.endswith('/hello'):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = '<html><body>Hello!</body></html>'
+                self._set_headers()
+                output = '<html><body>Hello!'
+                output += '<form method="POST" enctype="multipart/form-data" '
+                output += 'action="/hello">'
+                output += '<h2>What would you like me to say?</h2>'
+                output += '<input name="message" type="text">'
+                output += '<input type="submit" value="Submit">'
+                output += '</form>'
+                output += '</body></html>'
                 print(output)
                 response = bytes(output, 'UTF-8')
                 self.wfile.write(response)
             if self.path.endswith('/hola'):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = '<html><body>&#161Hola!<a href="/hello">Back to Hello</a></body></html>'
+                self._set_headers()
+                output = '<html><body>&#161Hola!'
+                output += '<a href="/hello">Back to Hello</a>'
+                output += '<form method="POST" enctype="multipart/form-data" '
+                output += 'action="/hello">'
+                output += '<h2>What would you like me to say?</h2>'
+                output += '<input name="message" type="text">'
+                output += '<input type="submit" value="Submit">'
+                output += '</form>'
+                output += '</body></html>'
                 print(output)
                 response = bytes(output, 'UTF-8')
                 self.wfile.write(response)
@@ -28,12 +45,28 @@ class webserverHandler(BaseHTTPRequestHandler):
         try:
             self.send_response(301)
             self.end_headers()
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                message_content = fields.get('message')
-        except:
-            pass
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={'REQUEST_METHOD': 'POST'}
+            )
+            print(form.getvalue("message"))
+            output = ''
+            output += '<html><body>'
+            output += '<h2>Okay,</h2>'
+            output += '<h1>{}</h1>'.format(form.getvalue("message"))
+            output += '<form method="POST" enctype="multipart/form-data" '
+            output += 'action="/hello">'
+            output += '<h2>What would you like me to say?</h2>'
+            output += '<input name="message" type="text">'
+            output += '<input type="submit" value="Submit">'
+            output += '</form>'
+            output += '</body></html>'
+            print(output)
+            response = bytes(output, 'UTF-8')
+            self.wfile.write(response)
+        except Exception as exc:
+            print(exc)
 
 def main():
     try:
