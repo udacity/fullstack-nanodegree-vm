3fbebb9c8b53eea759f97725b71b6ad93528fe35
Jared <fallt.jared@gmail.com>
2020-09-06 15:54:58 -0400

    add json serializing

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -7,11 +7,21 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -24,7 +34,19 @@ class MenuItem(Base):
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+
 
 engine = create_engine('sqlite:///restaurantmenu.db')
+
 
 Base.metadata.create_all(engine)
