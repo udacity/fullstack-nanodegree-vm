f40a9dae58d06da89d19380209b983fdc5b70f08
Yifei Yan <yifeiyan@mathworks.com>
2020-11-29 13:32:28 +0800

    Intro to relational database: python db api
    connect forum to db
    
    SQL injection bug fix
    
    fix script injection bug

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,27 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
+import bleach
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+dbname = "forum"
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = psycopg2.connect(database=dbname)
+  c = db.cursor()
+  c.execute("select content, time from posts order by time desc;")
+  posts = c.fetchall()
+  db.close()
+  return posts
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  db = psycopg2.connect(database=dbname)
+  c = db.cursor()
+  clean = bleach.clean(content)
+  c.execute("insert into posts values (%s)", (clean,))
+  db.commit()
+  db.close()
 
 
